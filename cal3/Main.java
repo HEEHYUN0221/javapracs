c3dda894fe2c84c19de058baf08044019032c4f9
HEEHYUN0221 <psr4580@gmail.com>
2024-11-18 14:38:27 +0900

    biggerSaveDatamethodmodify

--- cal3/Main.java
+++ cal3/Main.java
@@ -1,5 +1,7 @@
 package questcal.cal3;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,36 +10,51 @@ public class Main {
         double result = 0;
         Scanner sc = new Scanner(System.in);
         while (true) {
-            System.out.print("첫번째 숫자 입력 : ");
-            double a = sc.nextDouble();
-            System.out.print("두번째 숫자 입력 : ");
-            double b = sc.nextDouble();
-            if (a < 0 || b < 0) {
-                System.out.println("양의 정수를 입력해주세요.");
-                continue;
-            }
+            try {
+                System.out.print("첫번째 숫자 입력 : ");
+                double a = sc.nextDouble();
+                System.out.print("두번째 숫자 입력 : ");
+                double b = sc.nextDouble();
+
 
             System.out.print("연산자 입력 : ");
             System.out.println("ADD/SUB/MUL/DIV");
             String opr = sc.next();
 
-
             result= cal.calculate(opr,a,b);
 
             System.out.println("결과 값은 : " + result + "입니다.");
-            System.out.println("1. 결과저장 \n2. 결과 확인 \n3. 결과 제거 \n4. 나가기");
+            cal.setSaveData(opr,a,b);
+
+            ArrayList<Double> bre = cal.biggerSaveData(result);
+            System.out.println("a보다 큰 값들:");
+            if(bre.isEmpty()){
+                System.out.println("더 큰값이 없어요");
+            } else {
+                for (Double value : bre) {
+                    System.out.println(value);
+                }
+            }
+
+
+
+            System.out.println("1. 결과 확인 \n2. 결과 제거 \n3. 나가기\n 처음으로 돌아가시려면 아무 숫자나 입력해주세요.");
             String savey = sc.next();
             if(savey.equals("1")){
-                cal.setSaveData(opr,a,b);
-            }
-            else if(savey.equals("2")){
                 cal.getSaveData();
             }
-            else if(savey.equals("3")){
+            else if(savey.equals("2")){
                 cal.dataDelete();
             }
-            else if(savey.equals("4")) {
+            else if(savey.equals("3")){
                 break;
+            }
+
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("오류 발생" + e.getMessage());
             }
         }
     }
