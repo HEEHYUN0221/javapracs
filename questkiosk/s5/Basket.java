637200a23007725a7b451b1f907fbb0fbae04297
HEEHYUN0221 <psr4580@gmail.com>
2024-11-27 13:01:57 +0900

    hh

---
+++ questkiosk/s5/Basket.java
@@ -0,0 +1,64 @@
+package questcal.questkiosk.s5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Basket {
+    //메뉴이름, 수량, 가격
+    private HashMap<MenuItem, Integer> mapList;
+
+    public Basket() {
+        this.mapList=new HashMap<>();
+    }
+
+    public void addBasket(MenuItem selectItem) {
+        if(mapList.containsKey(selectItem)){
+            int tempValue = mapList.get(selectItem) +1;
+            mapList.replace(selectItem,tempValue);
+        } else {
+            mapList.put(selectItem,1);
+        }
+    }
+
+    //총 금액 계산
+    public double calAmount(){
+        double sumanswer = 0;
+        for(Map.Entry<MenuItem, Integer> entry : mapList.entrySet()){
+            MenuItem item = entry.getKey();
+            int quantity = entry.getValue();
+
+            sumanswer += (item.getMenuPrice()*1000)*quantity;
+        }
+
+        return sumanswer;
+    }
+
+    @Override
+    public String toString() {
+        if(mapList==null|| mapList.isEmpty()) {
+            return "장바구니가 비어있스미나.";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("장바구니 목록 : \n");
+        for(Map.Entry<MenuItem, Integer> entry : mapList.entrySet()){
+            MenuItem item = entry.getKey();
+            int quantity = entry.getValue();
+            sb.append("이름 : ");
+            sb.append(item.getMenuName());
+            sb.append(", 가격 : ");
+            sb.append(item.getMenuPrice()*1000);
+            sb.append("원 , 수량 : ");
+            sb.append(quantity);
+            sb.append("\n");
+        }
+        sb.append("총 금액 : ");
+        sb.append(calAmount());
+        return sb.toString();
+    }
+
+    public void allRemoveBasket() {
+        //장바구니 싹싹 비우기
+        mapList.clear();
+    }
+}
