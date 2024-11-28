cb807960177437cf57194b52a0efbd7f17f3cc18
HEEHYUN0221 <psr4580@gmail.com>
2024-11-28 10:42:04 +0900

    kioskmodify

--- questkiosk/s6/Kiosk.java
+++ questkiosk/s6/Kiosk.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Kiosk {
     private final List<Menu> menus = new ArrayList<>();
 
-    private Basket basket = new Basket();
+    private final Basket basket = new Basket();
 
     public Kiosk(Menu menus) {
         this.menus.add(menus);
@@ -17,8 +17,6 @@ public class Kiosk {
     public void addMenu(Menu menus) {
         this.menus.add(menus);
     }
-
-
 
 
     public void start() {
@@ -31,7 +29,7 @@ public class Kiosk {
                 for (int i = 0; i < menus.size(); i++) {
                     System.out.println(i + 1 + ". " + menus.get(i).getCategory());
                 }
-                System.out.println(0+ ".  종료");
+                System.out.println(0 + ".  종료");
 
                 int mainMenuInputNumber = scanner.nextInt();
                 //상위 카테고리 메뉴 선택 후 출력
@@ -41,14 +39,14 @@ public class Kiosk {
                 int inputMenuNumber = scanner.nextInt();
                 if (inputMenuNumber == 0) return;
                 //0입력시 종료
-                System.out.println("선택한 메뉴 : " + selectMenuCategory.getMenus().get(inputMenuNumber-1).toString());
+                System.out.println("선택한 메뉴 : " + selectMenuCategory.getMenus().get(inputMenuNumber - 1).toString());
 
 
                 //선택한 메뉴를 장바구니에 담을지 물어봐야함, 수량은 1번에 1개만 담을 수 있음
                 System.out.println("선택한 메뉴를 장바구니에 담으시겠습니까?");
                 String yorn = scanner.next();
-                if(yorn.equalsIgnoreCase("y")||yorn.equalsIgnoreCase("yes")){
-                    basket.addBasket(selectMenuCategory.getMenus().get(inputMenuNumber-1));
+                if (yorn.equalsIgnoreCase("y") || yorn.equalsIgnoreCase("yes")) {
+                    basket.addBasket(selectMenuCategory.getMenus().get(inputMenuNumber - 1));
                 }
                 //장바구니 목록 출력
                 System.out.println(basket);
@@ -56,7 +54,7 @@ public class Kiosk {
                 //주문할지 물어봐야함
                 System.out.println("주문을 완료하시겠습니까?");
                 String yornOrderComplete = scanner.next();
-                if(yornOrderComplete.equalsIgnoreCase("y")||yornOrderComplete.equalsIgnoreCase("yes")) {
+                if (yornOrderComplete.equalsIgnoreCase("y") || yornOrderComplete.equalsIgnoreCase("yes")) {
                     //할인할지 물어보는 부분 Discount Enum 사용
                     System.out.println("할인 정보를 입력해주세요.");
                     System.out.println("1. 국가 유공자 : 10%");
@@ -65,16 +63,15 @@ public class Kiosk {
                     System.out.println("4. 일반 : 0%");
                     int memberNumber = scanner.nextInt();
                     double discountTotalAmount = 0;
-                    for(Discount discount : Discount.values()) {
-                        if(discount.getIndex()==memberNumber) {
-                            discountTotalAmount= discount.getDiscount(basket.calAmount());
+                    for (Discount discount : Discount.values()) {
+                        if (discount.getIndex() == memberNumber) {
+                            discountTotalAmount = discount.getDiscount(basket.calAmount());
                         }
                     }
 
 
                     System.out.println("총 금액 : " + (int) discountTotalAmount);
                     basket.allRemoveBasket();
-                    break;
                 }
 
             } catch (InputMismatchException e) {
@@ -84,7 +81,7 @@ public class Kiosk {
                 System.out.println("범위에 벗어난 입력입니다. 다시 입력해주세요.");
                 scanner.nextLine();
             } catch (Exception e) {
-                System.out.println("알수없는 예외 : "+ e.getMessage());
+                System.out.println("알수없는 예외 : " + e.getMessage());
                 scanner.nextLine();
             }
         }
